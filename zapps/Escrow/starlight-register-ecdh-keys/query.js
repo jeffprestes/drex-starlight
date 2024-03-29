c1340e4a1e8190c5ac4c9947a238c7531301e3a7
Jeff Prestes <jeffprestes@gmail.com>
2024-03-29 16:15:00 -0500

    Create functions to allow user to register her ECDH keys in EscrowShield contract (#7)
    * chore: ignoring ECDH key
    
    * feat: register ECDH keys manually

--- zapps/Escrow/starlight-register-ecdh-keys/query.js
+++ zapps/Escrow/starlight-register-ecdh-keys/query.js
@@ -5,13 +5,15 @@ const erc20Abi = require('./ierc20.abi.json');
 
 // Infura provider
 const provider = new JsonRpcProvider(process.env.STARLIGHT_RPC_URL);
-const senderAddress = "";
-const receiverAddress = "";
+const senderAddress = "0x652DaCC13E9DaE920E6DD5e8460bECa6b42b71cc";
+const receiverAddress = "0x80D2BAa2b24c44A450e375B834D3a07845250476";
+const escrowShieldContractAddress = process.env.STARLIGHT_ESCROWSHIELD_ADDRESS
+const erc20ContractAddress = process.env.STARLIGHT_ERC20_ADDRESS;
 
 async function main() {
   // Define the contract
   console.log("process.env.STARLIGHT_RPC_URLs: ", process.env.STARLIGHT_RPC_URL);
-  const escrowShieldContract = new ethers.Contract("", abi, provider);
+  const escrowShieldContract = new ethers.Contract(escrowShieldContractAddress, abi, provider);
 
   const senderPubKey = await escrowShieldContract.zkpPublicKeys(senderAddress);
   console.log("senderPubKey: ", senderPubKey);
@@ -20,7 +22,7 @@ async function main() {
   console.log("receiverPubKey: ", receiverPubKey);
 
   console.log("============================");
-  const erc20Contract = new ethers.Contract("", erc20Abi, provider);
+  const erc20Contract = new ethers.Contract(erc20ContractAddress, erc20Abi, provider);
   
   let balance = await erc20Contract.balanceOf(senderAddress);
   console.log("Sender's ERC20 balance: ", balance.toString());
