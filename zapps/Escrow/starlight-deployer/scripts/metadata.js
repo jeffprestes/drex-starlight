5fd1de3fa013c5fbc6ed22c19d840979d0b69262
Jeff Prestes <jeffprestes@gmail.com>
2024-03-29 09:56:23 -0300

    feat: supporting multiple networks in the metadata file

--- zapps/Escrow/starlight-deployer/scripts/metadata.js
+++ zapps/Escrow/starlight-deployer/scripts/metadata.js
@@ -1,40 +1,64 @@
-const fs = require("fs");
+const fs = require('fs');
 
-function saveMetadata (
+function saveMetadata(
   contractDeployedAddress,
   contractName,
   networkId,
-  blockNumber
-  ) {
+  blockNumber,
+) {
   // console.log("__dirname", __dirname);
-  const parentDirectory = __dirname.substring(0, __dirname.lastIndexOf("/"));
-  // console.log("parentDirectory", parentDirectory);
-  
-  const hardhatArtifactContractPath = parentDirectory + "/artifacts/contracts";
-  // console.log("hardhatArtifactContractPath: ", hardhatArtifactContractPath);
-  const hardhatArtifactPath = hardhatArtifactContractPath + "/" + contractName + ".sol/" + contractName +".json";
-  // console.log("hardhatArtifactPath: ", hardhatArtifactPath);
-  
-  const compilationData = fs.readFileSync(hardhatArtifactPath, "utf-8");
-  const abi = JSON.parse(compilationData).abi;
-  const tmpData = {
-    abi: abi,
-    networks: {
-      [networkId] : {
-        address: contractDeployedAddress,
-        blockNumber: blockNumber
-      }
-    }
-  };
-  // console.log("tmpData: ", tmpData);
-  const buildFolder = __dirname.substring(0, __dirname.lastIndexOf("Escrow")) + "Escrow/build/contracts/";
+
+  const buildFolder =
+    __dirname.substring(0, __dirname.lastIndexOf('Escrow')) +
+    'Escrow/build/contracts/';
   // console.log("buildFolder: ", buildFolder);
-  const deployedFileName = buildFolder + contractName + "-metadata.json";
-  // console.log("Writing", deployedFileName, "...");
-  fs.writeFileSync(deployedFileName, JSON.stringify(tmpData));
-  // console.log(deployedFileName, " written with this content: ", JSON.stringify(tmpData));
+
+  const deployedFileName = buildFolder + contractName + '-metadata.json';
+
+  let deployedMetadata;
+  let deployedPositionMetadata = {
+    address: "",
+    blockNumber: 0,
+  }
+
+  // check if deployedFileName exists
+  if (fs.existsSync(deployedFileName)) {
+    const fileData = fs.readFileSync(deployedFileName, 'utf-8');
+    deployedMetadata = JSON.parse(fileData)
+  } else {
+    deployedMetadata = {
+      abi: {},
+      networks: {},
+    };
+  }
+  
+  const parentDirectory = __dirname.substring(0, __dirname.lastIndexOf('/'));
+  // console.log("parentDirectory", parentDirectory);
+
+  const hardhatArtifactContractPath = parentDirectory + '/artifacts/contracts';
+  // console.log("hardhatArtifactContractPath: ", hardhatArtifactContractPath);
+  const hardhatArtifactPath =
+    hardhatArtifactContractPath +
+    '/' +
+    contractName +
+    '.sol/' +
+    contractName +
+    '.json';
+  // console.log("hardhatArtifactPath: ", hardhatArtifactPath);
+
+  const compilationData = fs.readFileSync(hardhatArtifactPath, 'utf-8');
+  const abi = JSON.parse(compilationData).abi;
+
+  deployedMetadata.abi = abi;
+  deployedPositionMetadata.address = contractDeployedAddress;
+  deployedPositionMetadata.blockNumber = blockNumber;
+  deployedMetadata.networks[networkId] = deployedPositionMetadata;
+
+  console.log("Writing: ...");
+  fs.writeFileSync(deployedFileName, JSON.stringify(deployedMetadata));
+  console.log(deployedFileName, " written with this content: ", JSON.stringify(deployedMetadata));
 }
 
 module.exports = {
-  saveMetadata
-}
+  saveMetadata,
+};
