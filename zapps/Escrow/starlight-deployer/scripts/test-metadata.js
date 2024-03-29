1ffa9afe85a0b9aee4fe7d81965e18d56096a5ce
Jeff Prestes <jeffprestes@gmail.com>
2024-03-29 08:00:36 -0500

    Saves deployment metadata of multiple chains (#5)
    * feat: supporting multiple networks in the metadata file
    
    * chore: remove test file

--- zapps/Escrow/starlight-deployer/scripts/test-metadata.js
+++ zapps/Escrow/starlight-deployer/scripts/test-metadata.js
@@ -2,20 +2,18 @@ const hre = require("hardhat");
 const fs = require("fs");
 const saveMetadata = require("./metadata").saveMetadata;
 require("dotenv").config({ path: __dirname + "/.env" });
-const { BANKA_ADDRESS, ADMIN_ADDRESS } = process.env;
 
 async function main() {
   /*
   contractDeployedAddress,
   contractName,
   networkId,
-  transactionHash,
   blockNumber
   */
   saveMetadata(
     "0x39eCB99389433169fe175a4D2F9037D3F12c8699",
     "ERC20",
-    "1337",
+    "1",
     1
   );
 }
